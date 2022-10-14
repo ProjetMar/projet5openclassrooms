186722d23ec8ccda7abc79877b4fe655a7757438
marwa <marwamezghani29@gmail.com>
2022-10-14 23:04:00 +0200

    Vider le local storage quand la commande est passée

--- front/html/confirmation.js
+++ front/html/confirmation.js
@@ -2,3 +2,4 @@ let paramsString = window.location.search;
 let searchParams = new URLSearchParams (paramsString);
 const id = searchParams.get ("orderid");
 document.getElementById("orderId").innerText = id ; 
+localStorage.clear();
